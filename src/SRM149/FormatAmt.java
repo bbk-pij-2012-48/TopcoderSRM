dfa635daa341f5eaa6b4817f89bb285f9aaab82e
RBennett <rbennett485@gmail.com>
2013-03-11 11:41:17 +0000

    Rewritten using StringBuffer. Now passes all system tests

--- src/SRM149/FormatAmt.java
+++ src/SRM149/FormatAmt.java
@@ -1,35 +1,24 @@
 package SRM149;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FormatAmt {
 
 	public static String amount(int dollars, int cents) {
-		String result = "$";
-		if(dollars == 0) {
-			return result + "0." + cents;
+		
+		String result = Integer.toString(dollars);
+		StringBuffer tmp = new StringBuffer(result);
+		for(int i=result.length()-3 ; i>0 ; i=i-3) {
+			tmp.insert(i,',');
 		}
-		
-		List<Integer> tmp = new ArrayList<Integer>();
-		while(dollars != 0) {
-			tmp.add(dollars % 1000);
-			dollars /= 1000;
-		}
-		
-		for(int i=tmp.size()-1 ; i>=0 ; i--) {
-			result += tmp.get(i) + ",";
-		}
-		
-		result = result.substring(0,result.length()-1);
-		
-		if(cents<10) {
-			return (result += ".0" + cents);
+		tmp.insert(0,'$');
+		tmp.append('.');
+		if(cents>=10) {
+			tmp.append(cents);
 		} else {
-			return (result += "." + cents);
+			tmp.append('0');
+			tmp.append(cents);
 		}
+		return tmp.toString();
 	}
-	
 	public static void main(String[] args) {
 		System.out.println(amount(123456,67));
 
