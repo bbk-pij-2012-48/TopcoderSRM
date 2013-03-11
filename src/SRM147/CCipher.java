fcad474d020455b70db5bc62f723a7a381f82463
RBennett <rbennett485@gmail.com>
2013-03-11 11:12:43 +0000

    Fixed to loop round between Z and A. Now passes all system tests

--- src/SRM147/CCipher.java
+++ src/SRM147/CCipher.java
@@ -6,12 +6,15 @@ public class CCipher {
 		char[] tmp = cipherText.toCharArray();
 		for (int i=0 ; i<tmp.length ; i++) {
 			tmp[i] -= shift;
+			if(!Character.isLetter(tmp[i])){
+				tmp[i] += 26;
+			}
 		}
 		return new String(tmp);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(decode("VQREQFGT", 2));
+		System.out.println(decode("AAABBBCCC", 2));
 
 	}
 
